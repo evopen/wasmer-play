8ce75e606759ffb58998eba1a0078cd019531e4c
Yuanheng Li <520dhh@gmail.com>
2022-08-17 08:32:36 +0800

    initial wasi

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    // Create the `WasiEnv`.
+    let mut wasi_env = wasmer_wasi::WasiState::new("command-name")
+        .finalize()
+        .unwrap();
     let store = wasmer::Store::default();
+    let module = wasmer::Module::from_file(
+        &store,
+        "../wasi-plugin/target/wasm32-wasi/debug/wasi-plugin.wasm",
+    )
+    .unwrap();
+    let imports = wasi_env.import_object(&module).unwrap();
+    let instance = wasmer::Instance::new(&module, &imports).unwrap();
+    let start = instance.exports.get_function("_start").unwrap();
+    start.call(&[]).unwrap();
 }
